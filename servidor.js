f873bb58597d26ac394d937746de3b7a1bc95818
yenkarii <guilherme.antonio1702@gmail.com>
2026-09-17 09:53:27 -0300

    Update 3

--- servidor.js
+++ servidor.js
@@ -2,11 +2,23 @@ const express = require("express");
 
 const app = express();
 
+let ALUNOS = [
+    {id: 1, nome: "Giulia", curso: "Desenvolvimento de Sistemas"},
+    {id: 2, nome: "Vinicius", curso: "Redes de Computadores"},
+    {id: 3, nome: "Iago", curso: "Banco de Dados"},
+    {id: 4, nome: "Igor", curso: "Administração"},
+    {id: 5, nome: "Ana", curso: "Desenvolvimento de Sistemas"},
+];
+
 app.get("/", (req, res) =>{
     res.json({
         mensagem: "API Alunos funcionando."
     })
 });
+
+app.get("/alunos", (req, res) =>{
+    res.json(ALUNOS);
+})
 
 const PORTA = 3000;
 app.listen(PORTA, () => {

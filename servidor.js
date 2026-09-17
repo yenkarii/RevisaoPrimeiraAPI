bfacb2c9b8040f5662b7164baed79b1c9a238b58
yenkarii <guilherme.antonio1702@gmail.com>
2026-09-17 09:23:27 -0300

    Update 2

--- servidor.js
+++ servidor.js
@@ -1,13 +1,15 @@
 const express = require("express");
 
-express().get("/", (req, res) =>{
+const app = express();
+
+app.get("/", (req, res) =>{
     res.json({
         mensagem: "API Alunos funcionando."
     })
 });
 
 const PORTA = 3000;
-express().listen(PORTA, () => {
+app.listen(PORTA, () => {
     console.log("Servidor iniciado com sucesso");
     console.log(`http://localhost:${PORTA}`);
 })
